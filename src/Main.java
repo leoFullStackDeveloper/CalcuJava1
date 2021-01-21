b68fe35258891c80062fb6774afa563da766a966
leoFullStackDeveloper <leo.technology@hotmail.com>
2021-01-21 16:26:46 -0500

    adicionamos netodo sumar a la calculadora

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Calculator");
+
+        Calculator calculadora1 = new Calculator(10, 20);
+
+        System.out.println("La suma de los numeros 10 y 20 es : "+calculadora1.sumar());
+
     }
 }
